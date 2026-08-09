21ea2783d2cff81d7a7c99d82df2bea1943112cb
Dishant Panchal <dishantpanchal2004email@gmail.com>
2026-08-09 19:14:36 +0530

    chore: initialize JewelCore backend

---
+++ src/main/java/com/dishant/jewelcore/JewelcoreApplication.java
@@ -0,0 +1,13 @@
+package com.dishant.jewelcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dishant/jewelcore/JewelcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishant.jewelcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
